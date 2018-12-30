a4b3a4261cb693fb68ddd89e5249fdf78b8b3ee0
Alfredo Mendoza <almendoza25@gmail.com>
2018-12-30 12:53:03 -0800

    Checkpoint 6 Topics 2: completed

--- src/db/queries.topics.js
+++ src/db/queries.topics.js
@@ -2,16 +2,63 @@ const Topics = require("./models").Topics;
 
 module.exports = {
 
-//#1
   getAllTopics(callback){
-    return Topics.all()
+     return Topics.all()
+     .then((topics) => {
+       callback(null, topics);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+  },
+  getTopic(id, callback){
+     return Topics.findById(id)
+     .then((topic) => {
+       callback(null, topic);
+     })
+     .catch((err) => {
+       callback(err);
+     })
+  },
+  addTopic(newTopic, callback){
+      return Topics.create({
+        title: newTopic.title,
+        description: newTopic.description
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+  },
+  deleteTopic(id, callback){
+      return Topics.destroy({
+        where: {id}
+      })
+      .then((topic) => {
+        callback(null, topic);
+      })
+      .catch((err) => {
+        callback(err);
+      })
+  },
+  updateTopic(id, updatedTopic, callback){
+      return Topics.findById(id)
+      .then((topic) => {
+        if(!topic){
+          return callback("Topic not found");
+        }
 
-//#2
-    .then((topics) => {
-      callback(null, topics);
-    })
-    .catch((err) => {
-      callback(err);
-    })
+        topic.update(updatedTopic, {
+          fields: Object.keys(updatedTopic)
+        })
+        .then(() => {
+          callback(null, topic);
+        })
+        .catch((err) => {
+          callback(err);
+        });
+      });
   }
 }
