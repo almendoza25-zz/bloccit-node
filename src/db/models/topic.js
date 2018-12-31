2f37f6027f0594e2736acccc167008383824b069
Alfredo Mendoza <almendoza25@gmail.com>
2018-12-31 11:06:45 -0800

    Checkpoint 8 Post Part 1: Completed

--- src/db/models/topic.js
+++ src/db/models/topic.js
@@ -1,15 +1,25 @@
 'use strict';
 module.exports = (sequelize, DataTypes) => {
   var Topics = sequelize.define('Topics', {
-    title: DataTypes.STRING,
-    description: DataTypes.STRING
+    title: {
+       type: DataTypes.STRING,
+       allowNull: false
+    },
+    description: {
+       type: DataTypes.STRING,
+       allowNull: false
+    }
   }, {});
   Topics.associate = function(models) {
     // associations can be defined here
     Topics.hasMany(models.Banner, {
-     foreignKey: "topicId",
-     as: "banners",
-   });
+      foreignKey: "topicId",
+      as: "banners",
+    });
+    Topic.hasMany(models.Post, {
+      foreignKey: "topicId",
+      as: "posts"
+    });
   };
   return Topics;
 };
